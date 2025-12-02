0f46db754e5322b983d5e2bdeeefec88ef462d7c
Otabek <burhonovotabek5@gmail.com>
2025-12-02 08:17:23 +0500

    main

--- src/Admin/Main.jsx
+++ src/Admin/Main.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Main_admin() {
   return (
     <div className='w-full h-full bg-white dark:bg-slate-700 rounded-xl'>
-
+      <div className="news w-full h-max p-4 flex flex-col gap-4">
+        <h3 className="text-slate-700 dark:text-white text-3xl">Manage news</h3>
+      </div>
     </div>
   )
 }
